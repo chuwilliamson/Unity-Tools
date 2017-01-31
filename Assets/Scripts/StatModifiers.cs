141f03ba05de1135b72aaf898d58b396d79a1d34
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-01-31 16:11:20 -0600

    Stats
    Very simple cases for stats on each of the first possible jobs of a
    character

---
+++ Assets/Scripts/StatModifiers.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IStats
+{
+    int Attack { get; set; }
+    int Defense { get; set; }
+    float CriticalChance { get; set; }
+    float AttackSpeed { get; set; }
+
+    void CalculateAttack();
+    void CalculateDefense();
+    void CalculateCritChance();
+    void CalculateAtkSpeed();
+}
+
+public class StatModifiers : MonoBehaviour
+{
+    #region Fields
+    [SerializeField]
+    int mStrength;
+    [SerializeField]
+    int mStamina;
+    [SerializeField]
+    int mDexterity;
+    [SerializeField]
+    int mIntelligence;
+    #endregion
+
+    #region Properties
+    public int Strength
+    {
+        get { return mStrength; }
+    }
+
+    public int Stamina
+    {
+        get { return mStrength; }
+    }
+
+    public int Dexterity
+    {
+        get { return mDexterity; }
+    }
+
+    public int Intelligence
+    {
+        get { return mIntelligence; }
+    }
+    #endregion
+}
