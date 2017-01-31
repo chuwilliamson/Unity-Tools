b06c912dcf0566dc163f78c18a39f110fb86e879
Matthew Williamson <chuwilliamson@gmail.com>
2017-01-31 13:12:43 -0600

    Add classes and Behaviourmachine

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourMachine;
+public class PlayerController : MonoBehaviour
+{
+    public Blackboard blackboard;
+    // Use this for initialization
+    void Start()
+    {
+        blackboard = GetComponentInChildren<Blackboard>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha4))
+            blackboard.SendEvent(54644675);
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+            blackboard.SendEvent(913225611);
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+            blackboard.SendEvent(1346017622);
+        if(Input.GetKeyDown(KeyCode.Alpha3))
+            blackboard.SendEvent(590960297);
+    }
+}
