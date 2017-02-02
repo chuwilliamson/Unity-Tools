789d7e4a86aecf23874b31c3be40b28fd01ee420
Matthew Williamson <chuwilliamson@gmail.com>
2017-02-02 12:27:21 -0600

    Merge Dylan Add SubClasses

--- Assets/Scripts/Jobs/JobSelection.cs
+++ Assets/Scripts/Jobs/JobSelection.cs
@@ -2,26 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using BehaviourMachine;
+public enum JobState
+{
+ACROBAT = 54644675,    
+MERCENARY = 913225611,
+MAGICIAN = 1346017622,
+ASSIST = 590960297,
+        
+}
 public class JobSelection : StateBehaviour {
     
     void OnEnable()
     {
         
     }
-    void Process()
-    {
 
-    }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-            blackboard.SendEvent(54644675);
         if (Input.GetKeyDown(KeyCode.Alpha1))
-            blackboard.SendEvent(913225611);
+            blackboard.SendEvent((int)JobState.ACROBAT);
         if (Input.GetKeyDown(KeyCode.Alpha2))
-            blackboard.SendEvent(1346017622);
+            blackboard.SendEvent((int)JobState.MERCENARY);
         if (Input.GetKeyDown(KeyCode.Alpha3))
-            blackboard.SendEvent(590960297);
+            blackboard.SendEvent((int)JobState.MAGICIAN);
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            blackboard.SendEvent((int)JobState.ASSIST);
     }
     void OnDisable()
     {
