3e7c62cfd5f6616ba079dd1891822784fdd5f401
Matthew Williamson <chuwilliamson@gmail.com>
2017-02-02 13:40:38 -0600

    PlayerBehaviour callbacks

--- Assets/Scripts/FSMBlackboard.cs
+++ Assets/Scripts/FSMBlackboard.cs
@@ -7,11 +7,12 @@ using UnityEngine;
 using BehaviourMachine;
 
 public class FSMBlackboard : Blackboard {
+    // IntVars
+    public static readonly int Dexterity = 1054696003;
     // FsmEvents
     public static readonly int ACROBAT = 54644675;
     public static readonly int MERCENARY = 913225611;
     public static readonly int MAGICIAN = 1346017622;
     public static readonly int ASSIST = 590960297;
     public static readonly int LEVELUP = 1870230623;
-    public static readonly int BILLPOSTER = 986124621;
 }
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class EventOnLevelUp : UnityEvent
+{
+}
+public class PlayerBehaviour : MonoBehaviour
+{    
+    public void Awake()
+    {
+        onPlayerLevelUp = new EventOnLevelUp();
+    }
+
+    public void Start()
+    {
+        experience = 0;
+        level = 1;
+    }
+
+    public void GainExperience(int num)
+    {
+        experience += num;
+        var newLevel = (.5f * Mathf.Sqrt(experience));
+        if(newLevel > level)
+            onPlayerLevelUp.Invoke();
+        level = (int)newLevel;
+    }
+
+    public int experience;
+    public int level;
+    public EventOnLevelUp onPlayerLevelUp;
+}
